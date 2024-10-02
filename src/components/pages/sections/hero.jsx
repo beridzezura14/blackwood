36b9f59f4cbb66c2413b7d3b2ecab7e69d5484dd
beridzezura14 <zuraberidze12@gmail.com>
2024-10-02 14:26:16 +0400

    commit

--- src/components/pages/sections/hero.jsx
+++ src/components/pages/sections/hero.jsx
@@ -1,3 +1,5 @@
+import gsap from 'gsap'
+// import { useEffect } from 'react';
 
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
@@ -7,45 +9,65 @@ import hero1 from '../../../assets/img/hero1.jpeg'
 import hero2 from '../../../assets/img/hero2.jpg'
 import hero3 from '../../../assets/img/slider-2.jpg'
 
+const heroList = [
+    {
+        id: 1,
+        img: hero1,
+        name: "Living Room",
+        para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore reiciendis quae sunt sapiente sed minus quasi perferendis ea provident quis.",
+    },
+    {
+        id: 2,
+        img: hero2,
+        name: "Office Area",
+        para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore reiciendis quae sunt sapiente sed minus quasi perferendis ea provident quis.",
+    },
+    {
+        id: 3,
+        img: hero3,
+        name: "Hottel Part",
+        para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore reiciendis quae sunt sapiente sed minus quasi perferendis ea provident quis.",
+    },
+]
 
 
-function hero() {
+function Hero() {
 
-    const heroList = [
-        {
-            id: 1,
-            img: hero1,
-            name: "Living Room",
-            para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore reiciendis quae sunt sapiente sed minus quasi perferendis ea provident quis.",
-        },
-        {
-            id: 2,
-            img: hero2,
-            name: "Office Area",
-            para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore reiciendis quae sunt sapiente sed minus quasi perferendis ea provident quis.",
-        },
-        {
-            id: 3,
-            img: hero3,
-            name: "Hottel Part",
-            para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore reiciendis quae sunt sapiente sed minus quasi perferendis ea provident quis.",
-        },
-    ]
+    // const textRefs = useRef([]);
 
+    // useEffect(() => {
+    //     // ანიმაცია თითოეული სლაიდისთვის
+    //     gsap.fromTo(".fade",
+    //       { opacity: 0, x: -150 },
+    //       { opacity: 1, x: 0, duration: 1, stagger: 0.2 }
+    //     );
+    // }, []);
 
-    
 
     const slider = {
-        dots: false, // ქვემოთ წერტილების ჩვენება
+        dots: false, 
         arrows: true,
-        infinite: true, // უსასრულო სლაიდინგი
-        speed: 500, // სლაიდინგის სიჩქარე მილიწამებში
-        slidesToShow: 1, // რამდენი სლაიდი გამოჩნდება ერთდროულად
-        slidesToScroll: 1, // რამდენი სლაიდი გადაიშლება ერთხელ
-        autoplay: true, // ავტომატური სლაიდინგი
-        autoplaySpeed: 5000, // ავტომატური სლაიდინგის სიჩქარე
-      };
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1, 
+        slidesToScroll: 1, 
+        autoplay: true,
+        autoplaySpeed: 7000, 
+        beforeChange: () => {
+            // ანიმაცია სლაიდის ცვლილებაზე
+            gsap.fromTo('.fade',
+              { opacity: 0, y: 150, stagger: .1, ease: "power1.in"},
+              { opacity: 1, y: 0, duration: 1, stagger: .1, ease: "power1.in"}
+            );
+            // gsap.fromTo('.btn', 
+            //   { delay: 1, opacity: 0, },
+            //   { delay: 1, opacity: 1, duration: 1}
+            // )
+          },
+    };
     
+
+
     return (
         <div className='main__hero'>
 
@@ -54,13 +76,28 @@ function hero() {
                         heroList.map((item) => (
                             <div key={item.id}> 
                                 <div style={{
-                                    height: "80dvh",
+                                    height: "92dvh",
                                     width: "100%",
                                     backgroundColor: "grey",
+                                    display: "flex",
+                                    // justifyContent: "center",
+                                    alignItems: 'center',
+                                    backgroundImage: `linear-gradient(#0000009a, #000000c9),
+                                    url(${item.img})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    overflowX: "hidden"
                                 }}>
-                                    <div className='hero__text'>
-                                        <h1>{item.name}</h1>
-                                        <p>{item.para}</p>
+                                    <div  className='hero__text'>
+                                        <div className='overflow'>
+                                            <h1 className="fade">{item.name}</h1>
+                                        </div>
+                                        <div className='overflow'>
+                                            <p className="fade">{item.para}</p>
+                                        </div>
+                                        <div className='overflow'>
+                                            <button className='btn fade'>Details</button>    
+                                        </div>                                        
                                     </div>
                                 </div>
                             </div>
@@ -69,8 +106,8 @@ function hero() {
                 </Slider>
 
         </div>
-
     )
+    
 }
 
-export default hero
+export default Hero
